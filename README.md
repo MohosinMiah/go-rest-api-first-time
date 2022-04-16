64a4ce0617f07999d903ed590e15758a4fc3389e
mohosin16103308 <hamza1610330816@gmail.com>
2022-04-16 15:40:15 +0600

    Create New Country

--- README.md
+++ README.md
@@ -3,5 +3,9 @@
 <h3> HTTP server using Gorilla Mux <h3>
 <p> Github :  <a href="https://github.com/gorilla/mux"> Gorilla Mux </a> </p>
 <p>For include Gorilla Mux in my project :  <code>go get -u github.com/gorilla/mux</code> </p>  
+<h2 style="text-color:blue;font-weight:300">CRUD</h2>
+<h3> Added Create Functionality </h3>
+<p>Postman create new country img: <a href="https://drive.google.com/file/d/1jWGB93I7ooeyXn6XF8smHfbreioIy-AX/view?usp=sharing"> <p>
+
 
 
--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 
@@ -9,33 +11,48 @@ import (
 )
 
 type country struct {
-	ID          string `json:"ID"`
-	Name        string `json:"Name"`
-	Description string `json:"Description"`
+	ID          string `json:"id"`
+	Name        string `json:"name"`
+	Description string `json:"description"`
 }
 
 type allCountries []country
 
-var countrys = allCountries{
+var countries = allCountries{
 	{
 		ID:          "1",
 		Name:        "Bangladesh",
 		Description: "Bangladesh is a very beautiful country surrounding with river and natural beauty",
 	},
-	{
-		ID:          "2",
-		Name:        "India",
-		Description: "India is a very dynamic economic and multicultural country. I have a plan to visit India",
-	},
-	{
-		ID:          "2",
-		Name:        "USA",
-		Description: "Most young people dream to study in USA. It's beautiful country",
-	},
+	// {
+	// 	ID:          "2",
+	// 	Name:        "India",
+	// 	Description: "India is a very dynamic economic and multicultural country. I have a plan to visit India",
+	// },
+	// {
+	// 	ID:          "2",
+	// 	Name:        "USA",
+	// 	Description: "Most young people dream to study in USA. It's beautiful country",
+	// },
+}
+
+// Create a New Country
+func createEvent(w http.ResponseWriter, r *http.Request) {
+	var newCountry country
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(w, "Create a new country .Please Insert Country Name and Description.")
+	}
+
+	json.Unmarshal(reqBody, &newCountry)
+	countries = append(countries, newCountry)
+	w.WriteHeader(http.StatusCreated)
+
+	json.NewEncoder(w).Encode(newCountry)
 }
 
 func HomePage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, I am FRom Home Routing!")
+	fmt.Fprintf(w, "Hello, I am From Home Routing!")
 }
 
 func main() {
@@ -47,6 +64,9 @@ func main() {
 
 	// Home Route , Url = /
 	router.HandleFunc("/", HomePage)
+
+	// Create a New Country Server
+	router.HandleFunc("/create", createEvent).Methods("POST")
 
 	// server setup and Running Server : 8080 port
 	log.Fatal(http.ListenAndServe(":8080", router))
