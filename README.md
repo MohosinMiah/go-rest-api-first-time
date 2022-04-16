8ee9fb1ae4d52d28336fefd3559f11ea73769bcf
mohosin16103308 <hamza1610330816@gmail.com>
2022-04-16 13:53:08 +0600

    Home Page Router Setup Complete

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 <h1> Rest API using Golan using static data<h1> 
+
+<h3> HTTP server using Gorilla Mux <h3>
+<p> Github :  <a href="https://github.com/gorilla/mux"> Gorilla Mux </a> </p>
+<p>For include Gorilla Mux in my project :  `go get -u github.com/gorilla/mux` </p>  
+
+
--- main.go
+++ main.go
@@ -1,8 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func HomePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, I am FRom Home Routing!")
+}
 
 func main() {
-	fmt.Println("Hello World!")
-	fmt.Println("Hello Welcome From Bangladesh")
+
+	fmt.Println("Hello World")
+
+	// Initialize the mux router
+	router := mux.NewRouter().StrictSlash(true)
+
+	// Home Route , Url = /
+	router.HandleFunc("/", HomePage)
+
+	// server setup and Running Server : 8080 port
+	log.Fatal(http.ListenAndServe(":8080", router))
+
 }
