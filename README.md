98e00bb59c4afd03f10cc8159d8692e34766a16b
mohosin16103308 <hamza1610330816@gmail.com>
2022-04-16 14:00:25 +0600

    Demo Data Added

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 <h3> HTTP server using Gorilla Mux <h3>
 <p> Github :  <a href="https://github.com/gorilla/mux"> Gorilla Mux </a> </p>
-<p>For include Gorilla Mux in my project :  `go get -u github.com/gorilla/mux` </p>  
+<p>For include Gorilla Mux in my project :  <code>go get -u github.com/gorilla/mux</code> </p>  
 
 
--- main.go
+++ main.go
@@ -8,6 +8,32 @@ import (
 	"github.com/gorilla/mux"
 )
 
+type country struct {
+	ID          string `json:"ID"`
+	Name        string `json:"Name"`
+	Description string `json:"Description"`
+}
+
+type allCountries []country
+
+var countrys = allCountries{
+	{
+		ID:          "1",
+		Name:        "Bangladesh",
+		Description: "Bangladesh is a very beautiful country surrounding with river and natural beauty",
+	},
+	{
+		ID:          "2",
+		Name:        "India",
+		Description: "India is a very dynamic economic and multicultural country. I have a plan to visit India",
+	},
+	{
+		ID:          "2",
+		Name:        "USA",
+		Description: "Most young people dream to study in USA. It's beautiful country",
+	},
+}
+
 func HomePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, I am FRom Home Routing!")
 }
